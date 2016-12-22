dc77e3b42e6655674fb7a99782c215de0e0a3799
cajoue <cajoue@users.noreply.github.com>
2016-12-22 14:52:03 +0100

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{name}}</h1>
+    <p><strong>Email:</strong> {{email}}</p>
+    <p><strong>Address: </strong>{{address.street}}, {{address.city}}, {{address.canton}}</p>
+    `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Coucou';
+  email = 'coucou@example.com';
+  address = {
+    street: '12 Main street',
+    city: 'Lausanne',
+    canton: 'Vaud'
+  };
+}
