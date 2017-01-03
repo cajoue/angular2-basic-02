7d4a77ec0596f59d910afbdd1d338f062218c36b
cajoue <cajoue@users.noreply.github.com>
2017-01-03 10:44:58 +0100

    feat: add hobbies use interface *ngFor *ngIf

--- app/components/user.component.ts
+++ app/components/user.component.ts
@@ -6,14 +6,38 @@ import { Component } from '@angular/core';
     <h1>Hello {{name}}</h1>
     <p><strong>Email:</strong> {{email}}</p>
     <p><strong>Address: </strong>{{address.street}}, {{address.city}}, {{address.canton}}</p>
+    <div *ngIf="showHobbies">
+      <h3>Hobbies</h3>
+      <ul>
+        <li *ngFor="let hobby of hobbies">
+          {{hobby}} </li>
+      </ul>
+    </div>
     `,
 })
 export class UserComponent  {
-  name = 'Coucou';
-  email = 'coucou@example.com';
-  address = {
-    street: '12 Main street',
-    city: 'Lausanne',
-    canton: 'Vaud'
-  };
+  name: string;
+  email: string;
+  address: address;
+  hobbies: string[];
+  showHobbies: boolean;
+
+  constructor(){
+    console.log('constructor ran');
+    this.name = 'Coucou';
+    this.email = 'coucou@example.com';
+    this.address = {
+      street: '12 Main street',
+      city: 'Lausanne',
+      canton: 'Vaud'
+    };
+    this.hobbies = ['Music', 'Movies', 'Code'];
+    this.showHobbies = false;
+  }
+}
+
+interface address {
+  street: string;
+  city: string;
+  canton: string;
 }
